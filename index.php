46893b4bdb66d2ef27add195f9ab34eb279c560f
AjvorHardi <mravsteen@gmail.com>
2022-03-01 12:40:58 +0100

    Add nav bar

--- index.php
+++ index.php
@@ -13,6 +13,17 @@
 
 <body>
 
+    <nav>
+        <div class="container">
+            <a href="index.php" class="logo">Creative</a>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="team.php">Team</a></li>
+                <li><a href="projects.php">Projects</a></li>
+            </ul>
+        </div>
+    </nav>
+
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <script src="js/owl.carousel.min.js"></script>
     <script src="js/main.js"></script>
