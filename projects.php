25ca50f3c82eb36cde5929c23edb7a0f5316b405
AjvorHardi <mravsteen@gmail.com>
2022-03-01 21:25:10 +0100

    Finish projects page

--- projects.php
+++ projects.php
@@ -1,6 +1,30 @@
 
 <?php include 'single-headers.php'; ?>
 
-<h1>Projects</h1>
+<section class="projects container">
+        <article>
+            <h2 class="headings">Projects</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus consectetur beatae accusantium fuga consequuntur, non, culpa eius voluptas assumenda necessitatibus aliquid vel error quidem temporibus inventore dicta amet explicabo cum esse. Maiores quos rerum totam, delectus doloribus incidunt exercitationem atque quidem laudantium, molestiae nobis quia ratione dolor repudiandae nesciunt unde.</p>
+        </article>
+        <article>
+            <img src="img/projects.jpg" alt="">
+        </article>
+</section>
+
+<section class="slider">
+    <div class="owl-carousel owl-theme">
+        <div class="item"><img src="img/works1.jpg" alt=""></div>
+        <div class="item"><img src="img/works2.jpg" alt=""></div>
+        <div class="item"><img src="img/works3.jpg" alt=""></div>
+        <div class="item"><img src="img/works4.jpg" alt=""></div>
+        <div class="item"><img src="img/works5.jpg" alt=""></div>
+        <div class="item"><img src="img/works6.jpg" alt=""></div>
+        <div class="item"><img src="img/works7.jpg" alt=""></div>
+        <div class="item"><img src="img/works8.jpg" alt=""></div>
+        <div class="item"><img src="img/works9.jpg" alt=""></div>
+        <div class="item"><img src="img/works10.jpg" alt=""></div>
+    </div>
+</section>
+    
 
 <?php include 'footer.php'; ?>
