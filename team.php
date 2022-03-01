db7571f2781ee702328112dc29663ef9d9d3776d
AjvorHardi <mravsteen@gmail.com>
2022-03-01 20:13:51 +0100

    Add team page

--- team.php
+++ team.php
@@ -1,6 +1,28 @@
 
 <?php include 'header.php'; ?>
 
-<h1>Team</h1>
+<section class="team container">
+    <article>
+        <h2 class="headings">Our Creative Team</h2>     
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, beatae vitae debitis molestiae odio tempore porro deserunt illum, suscipit veritatis ut at reiciendis ducimus corporis animi soluta? Sint, repellendus temporibus!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus, beatae vitae debitis molestiae odio tempore porro deserunt illum, suscipit veritatis ut at reiciendis ducimus corporis animi soluta? Sint, repellendus temporibus!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure et doloremque minus quam, iusto eveniet, magni maxime eos necessitatibus ab temporibus officia consectetur sit nobis in nulla, dolorum vero quisquam quo facilis eum voluptates fugit? Similique, libero beatae tempore voluptatem maiores necessitatibus, laboriosam dolores modi fugit explicabo neque hic, ab delectus dignissimos amet cum mollitia. Aut delectus aperiam illum! Laborum.</p>
+        <button>Learn more</button>
+    </article>
+    <article>
+        <div>
+            <img src="img/team1.jpg" alt="">
+        </div>
+        <div>
+            <img src="img/team2.jpg" alt="">
+        </div>
+        <div>
+            <img src="img/team3.jpg" alt="">
+        </div>
+        <div>
+            <img src="img/team4.jpg" alt="">
+        </div>
+    </article>
+</section>
 
 <?php include 'footer.php'; ?>
